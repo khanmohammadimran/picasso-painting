203f315dd07a496042de6ee9671888b12c249ef2
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-05-25 17:18:40 +0600

    Minimum and available quantity error showed in the UI

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import Loading from '../Shared/Loading';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Purchase = () => {
     const { purchaseId } = useParams();
-    const [tools, setTools, qError] = useState({});
+    const [tools, setTools] = useState({});
     const [user] = useAuthState(auth);
-    let { minimumOrderQuantity } = tools;
+    let { minimumOrderQuantity, availableQuantity } = tools;
 
     useEffect(() => {
         const url = `http://localhost:5000/tools/${purchaseId}`
@@ -19,14 +20,16 @@ const Purchase = () => {
             .then(data => setTools(data));
     }, [])
 
+
     const handleSubmit = event => {
         event.preventDefault();
         let number = event.target.number.value;
         number = parseInt(number)
         console.log(number)
         minimumOrderQuantity = parseInt(minimumOrderQuantity);
-        if (minimumOrderQuantity > number) {
-            return qError = <div className='text-red-500'> Can not order</div>
+        availableQuantity = parseInt(availableQuantity);
+        if (minimumOrderQuantity > number || availableQuantity < number) {
+            return toast.error('Please order atleast Minimum Quantity')
         }
         else {
             const url = `http://localhost:5000/tools/${purchaseId}`
@@ -57,12 +60,14 @@ const Purchase = () => {
         <div className="py-8">
             <div className='bg-white w-1/2 mx-auto rounded-lg drop-shadow-2xl p-2'>
                 <h2 className='text-center font-raleway text-3xl py-5'>See The Detail of the Tool</h2>
+                <h2 className='text-center font-raleway text-3xl py-5'>{user.displayName}</h2>
+                <h2 className='text-center font-raleway text-3xl py-5'>{user.email}</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 items-center bg-white'>
                     <div>
                         <img className='sm:object-cover mx-auto md:w-96 text-center p-3 rounded-lg' src={tools.img} alt="" />
                     </div>
                     <div className='font-sans'>
-                        <h2 className='pb-1 text-2xl'>Name: {tools.name}</h2>
+                        <h2 className='pb-1 text-2xl'>Tool Name: {tools.name}</h2>
                         <hr />
                         <p className='pb-1 text-2xl'>Price: ${tools.price}</p>
                         <hr />
@@ -78,7 +83,7 @@ const Purchase = () => {
                 <div className='text-center pb-4'>
                     <div className="relative flex py-5 items-center">
                         <div className="flex-grow border-t border-lime-500"></div>
-                        <span onChange={qError} className="flex-shrink mx-4 font-playfair text-2xl">Increase Quantity</span>
+                        <span className="flex-shrink mx-4 font-playfair text-2xl">Increase Quantity</span>
                         <div className="flex-grow border-t border-lime-500"></div>
                     </div>
                     <form onSubmit={handleSubmit}>
@@ -86,6 +91,7 @@ const Purchase = () => {
                         <br />
                         <input type="submit" value="Purchase" className='bg-lime-500 hover:bg-lime-600 rounded-lg w-1/2 py-2 text-white text-2xl' />
                     </form>
+                    <ToastContainer></ToastContainer>
                 </div>
             </div>
         </div>
